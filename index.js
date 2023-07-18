3c98e6b25b45bc227082847522fd105c24dc75dd
SmartKid2010637 <138292747+SmartKid2010637@users.noreply.github.com>
2023-07-18 17:14:56 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-var http = require('http'); // HTTP module
-var url = require('url'); // url module
-var fs = require('fs'); // file system module
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
 
 http.createServer(function (req, res) {
+  var q = url.parse(req.url, true);
+  var filename = "." + q.pathname;
+  fs.readFile(filename, function(err, data) {
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
+    } 
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write("Written by SmartKid");
-    res.end()
+    res.write(data);
+    return res.end();
+  });
 }).listen(8080);
